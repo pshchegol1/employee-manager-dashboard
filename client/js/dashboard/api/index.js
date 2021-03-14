66a209600c2dabb78d18244a465ba0f400ca8fdb
pshchegol1 <46579720+pshchegol1@users.noreply.github.com>
2021-03-13 19:06:03 -0700

    changed base url

--- client/js/dashboard/api/index.js
+++ client/js/dashboard/api/index.js
@@ -2,14 +2,14 @@
 // developmentUrl = 'http://localhost:5000/
 
 const api = {
-    getAllDepartments: 'http://localhost:5000/api/departments/',
+    getAllDepartments: 'https://employee-managers.herokuapp.com/api/departments/',
     // Snippet #5
-    updateEmployee: `http://localhost:5000/api/departments/employee/update`,
+    updateEmployee: `https://employee-managers.herokuapp.com/api/departments/employee/update`,
     getDepartment: (name)=>{
-        return `http://localhost:5000/api/departments/name/${name}`
+        return `https://employee-managers.herokuapp.com/api/departments/name/${name}`
     },
     deleteEmployee: (id)=>{
-        return `http://localhost:5000/api/departments/employee/id/${id}` 
+        return `https://employee-managers.herokuapp.com/api/departments/employee/id/${id}` 
     }
 }
 
